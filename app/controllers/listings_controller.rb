fdbb6ba8455c3fc3c0ec967f766d57af5e6a18d7
matthieu jacquier <matthieu.pro.jacquier@gmail.com>
2025-06-05 09:29:46 +0200

    search with album name & album artist

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -2,8 +2,10 @@ class ListingsController < ApplicationController
 
   def index
     @listings = Listing.all
-    if params[:query].present?
-    @listings = @listings.where(album_name: params[:query])
+   if params[:query].present?
+    sql_subquery = "album_name ILIKE :query OR artist_name ILIKE :query"
+    @listings = @listings.where(sql_subquery, query: "%#{params[:query]}%")
+
   end
 
     @markers = @listings.map do |listing|
