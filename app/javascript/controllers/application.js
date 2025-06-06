733459a6eecf6bc67a1a6120447e68bba1ce7043
matthieu jacquier <matthieu.pro.jacquier@gmail.com>
2025-06-06 16:21:18 +0200

    heroku push

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,17 @@
 import { Application } from "@hotwired/stimulus"
+import flatpickr from "flatpickr"
 
+// Initialize Stimulus
 const application = Application.start()
-
-// Configure Stimulus development experience
 application.debug = false
-window.Stimulus   = application
+window.Stimulus = application
+
+// Apply Flatpickr after each page load (works with Turbo)
+document.addEventListener("turbo:load", () => {
+  flatpickr(".datepicker", {
+    enableTime: false,
+    dateFormat: "Y-m-d"
+  });
+});
 
 export { application }
